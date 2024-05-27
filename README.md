72f54782a8c9f800b61c9d4c21005e255c86e6b3
KhizerWHussain <khizwaseem@gmail.com>
2024-05-27 02:05:22 -0700

    fix secret leak

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 1. clone the project and run command yarn or npm install to install node_modules
 
 2. yarn dev or npm run dev to run the development server
+
+
+project is created in next.js 14 version with plain module css
+
+and both backend & frontend
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,24 +2,16 @@ import mongoose from "mongoose";
 
 const connection = {};
 
-console.log(process.env.MONGO);
-
 export const connectToDb = async () => {
   try {
     if (connection?.isConnected) {
       console.log("using existing connection");
       return;
     }
-
-    // const db = await mongoose.connect(process.env.MONGO);
-    const db = await mongoose.connect(
-      "mongodb+srv://admin:admin@khizerapi.5wsywx6.mongodb.net/next_project?retryWrites=true&w=majority&appName=KhizerAPI"
-    );
-
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db?.connections[0]?.readyState;
     console.log("db connection established");
   } catch (error) {
-    console.log(error);
-    // throw new Error(`error connecting with database -> ${error}`);
+    throw new Error(`error connecting with database -> ${error}`);
   }
 };
